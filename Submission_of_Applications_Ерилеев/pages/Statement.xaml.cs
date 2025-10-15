d5f236b5b582fcc03f5db461a7ff77665e67054b
Денис Ерилеев <deniserileev85@gmail.com>
2025-10-15 15:59:59 +0300

    Созданная страница Statement. Фамилия

--- Submission_of_Applications_Ерилеев/pages/Statement.xaml.cs
+++ Submission_of_Applications_Ерилеев/pages/Statement.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Submission_of_Applications_Ерилеев
+namespace Submission_of_Applications_Ерилеев.pages
 {
     /// <summary>
     /// Логика взаимодействия для Statement.xaml
